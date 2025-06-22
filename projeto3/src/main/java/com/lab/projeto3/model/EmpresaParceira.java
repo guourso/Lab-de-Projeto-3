9dfc7b9841fcd67948b95b81a27f3644c446a7ea
Gustavo Henrique <guns_look@live.com>
2025-06-22 12:01:18 -0300

    remove id field from Aluno, EmpresaParceira, and Professor classes

--- projeto3/src/main/java/com/lab/projeto3/model/EmpresaParceira.java
+++ projeto3/src/main/java/com/lab/projeto3/model/EmpresaParceira.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Data;
@@ -19,9 +16,6 @@ import lombok.EqualsAndHashCode;
 @PrimaryKeyJoinColumn(name = "usuario_id")
 public class EmpresaParceira extends Usuario {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @Column(nullable = false, unique = true)
     private String cnpj;
--- projeto3/src/main/java/com/lab/projeto3/model/Professor.java
+++ projeto3/src/main/java/com/lab/projeto3/model/Professor.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrimaryKeyJoinColumn;
@@ -19,10 +16,6 @@ import lombok.EqualsAndHashCode;
 @Entity
 @PrimaryKeyJoinColumn(name = "usuario_id")
 public class Professor extends Usuario {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @Column(nullable = false, unique = true)
     private String cpf;
